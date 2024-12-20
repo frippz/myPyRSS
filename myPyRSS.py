404627099e404f32ef6bfe4f5c77bdc141600478
Fredrik Frodlund <fredrik@frodlund.se>
2024-12-20 08:04:53 +0100

    feat: numbered entries

--- myPyRSS.py
+++ myPyRSS.py
@@ -2,18 +2,19 @@ def main():
     import feedparser
     from bs4 import BeautifulSoup
 
-
     feed = feedparser.parse("https://frippz.se/feed.xml")
 
-    for entry in feed.entries[:2]:
+    for i, entry in enumerate(feed.entries[:10], start=1):
 
         soup = BeautifulSoup(entry.description, "html.parser")
         asText = soup.get_text()
         summary = asText[:200]
 
-        print('-')
-        print('Title: ',entry.title)
-        print('Summary:\n', summary, '…')
+        print(f"#{i}")
+        print("---")
+        print("Title: ", entry.title)
+        print("Summary:\n", summary, "…\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
